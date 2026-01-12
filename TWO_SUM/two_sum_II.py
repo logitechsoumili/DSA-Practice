479867577f156fb01634a58788eaba213df1752f
Soumili Saha <logitechsoumili@gmail.com>
2026-01-12 21:39:08 +0530

    .

--- TWO_SUM/two_sum_II.py
+++ TWO_SUM/two_sum_II.py
@@ -2,16 +2,13 @@
 167. Two Sum II - Input Array Is Sorted
 
 Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
-
 Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
-
 The tests are generated such that there is exactly one solution. You may not use the same element twice.
-
 Your solution must use only constant extra space.
 '''
 
-# BRUTE FORCE SOLUTION (O(n^2); unsorted/sorted)
-'''
+# BRUTE FORCE SOLUTION (O(n^2) time, O(1) space; unsorted/sorted)
+''' 
 def brute_force(arr, target):
     sum_ = 0
     for i in range(len(arr)):
@@ -21,7 +18,7 @@ def brute_force(arr, target):
     return None
 '''
 
-# BETTER SOLUTION (O(n); unsorted/sorted)
+# BETTER SOLUTION – Hashmap (O(n) time, O(n) space; unsorted/sorted)
 '''
 def better(arr, target):
     index_map = {}
@@ -32,7 +29,9 @@ def better(arr, target):
         index_map[val] = i
 '''
 
-# OPTIMISED SOLUTION (O(n) -> sorted; O(nlogn) -> unsorted)
+# OPTIMISED SOLUTION – Two Pointers (O(n) time, O(1) space -> sorted)
+# If the array is unsorted, sorting first results in O(n log n) time
+
 def optimised(arr, target):
     i, j = 0, len(arr) - 1
     while i < j:
