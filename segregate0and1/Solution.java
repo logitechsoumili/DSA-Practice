4ef3fb18cdef99f8bd5e10359a9b3e3c9b497a34
Soumili Saha <logitechsoumili@gmail.com>
2026-01-18 15:02:30 +0530

    fixed user input

--- segregate0and1/Solution.java
+++ segregate0and1/Solution.java
@@ -19,7 +19,7 @@ class Solution {
         int n = sc.nextInt();
         int[] arr = new int[n];
         
-        System.out.println("Enter sorted array: ");
+        System.out.println("Enter array: ");
         for (int i = 0; i < n; i++)
             arr[i] = sc.nextInt();
 
